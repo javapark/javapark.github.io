63f953bbd9c62cb3d51039b9405f58c6ac8765da
javapark <javaparknet@gmail.com>
2021-03-07 22:28:55 +0900

    disqus layout 에 자동으로 추가

---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,17 @@
+// .vuepress/theme/Layout.vue
+<template>
+    <ParentLayout >
+        <Disqus slot="page-bottom" v-if="this.$frontmatter.disqus" class="content"/>
+    </ParentLayout>
+</template>
+
+<script>
+    import ParentLayout from '@parent-theme/layouts/Layout.vue'
+    import Disqus from '../components/Disqus'
+    export default {
+        components: {
+            ParentLayout,
+            Disqus
+        }
+    }
+</script>
---
+++ docs/.vuepress/theme/index.js
@@ -0,0 +1,5 @@
+// .vuepress/theme/index.js
+// .vuepress/theme 폴더를 만들어주자.
+module.exports = {
+    extend: '@vuepress/theme-default'
+}
