c4e6cdc326720ecfdc462fd2402afca8fdfeec3c
javapark <javaparknet@gmail.com>
2022-10-26 19:03:52 +0900

    vuepress

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -8,7 +8,7 @@ const SEO = ({ title, description, url }) => {
       <title>{title}</title>
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
-      <meta property="og:image" content={`${siteUrl}/og-image.png`} />
+      <meta property="og:image" content={`${siteUrl}/og-image.jfif`} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
     </Helmet>
