ace2f8dc7b1ee6a4b6713b5004e1b04374753e00
javapark <javaparknet@gmail.com>
2022-10-31 20:47:46 +0900

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const blogConfig = require("./blog-config")
 const { title, description, author, siteUrl } = blogConfig
 
 module.exports = {
-  pathPrefix: "/gatsby-starter-hoodie",
+  pathPrefix: "/",
   siteMetadata: {
     title,
     description,
