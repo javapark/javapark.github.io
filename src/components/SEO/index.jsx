6d01e8e3a7f0020a8a8e685fbb036b51d99ac340
Javapark <javapark@users.noreply.github.com>
2023-09-09 10:04:30 +0900

    Update index.jsx

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { siteUrl } from "../../../blog-config"
+import { gtag } from "../../../blog-config"
 
 const SEO = ({ title, description, url }) => {
   return (
@@ -8,9 +9,18 @@ const SEO = ({ title, description, url }) => {
       <title>{title}</title>
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
-      <meta property="og:image" content={`${siteUrl}/og-image.jfif`} />
+      <meta property="og:image" content={`${siteUrl}/og-image.png`} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
+      <script
+        async
+        src={"https://www.googletagmanager.com/gtag/js?id=" + gtag}
+      ></script>
+      <script>
+        {`window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', '`+ gtag + `');`}
+      </script>
     </Helmet>
   )
 }
