b6eb2642cd4ef4c8d589eb288dc97cab24513b98
Javapark <javapark@users.noreply.github.com>
2023-09-13 10:30:58 +0000

    error fix

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "JavaPark",
   description: "Welcome to Javapark blog!!",
   author: "JavaPark",
-  siteUrl: "https://javapark.github.io/",
+  siteUrl: "",
   links: {
     github: "https://github.com/javapark",
     linkedIn: "https://linkedin.com",
